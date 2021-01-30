eb755478ea24f2c68ccdb8f4ef33e6d1bc1583f7
mohamedlotfe <mhmdlotfy9@gmail.com>
2021-01-30 12:08:49 +0200

    update

---
+++ README.md
@@ -0,0 +1,16 @@
+# Book-Backend-Prompt
+A simple server side application uisng Async prompt inputs and ouputs 
+
+
+## Install Dependencies
+
+Run `npm install` to install the project dependencies  
+
+
+## Run server
+
+Run `npm start` for a dev server. 
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 const managBook = require('../core/business/manage-book');
 
 /* GET home page. */
-router.get('/a', async function (req, res, next) {
+router.get('/books', async function (req, res, next) {
   //res.json(await managBook.ViewAllBook())
   // res.json(await managBook.getBookById("2"))
   // res.json(await managBook.addBook("book 4","abdallah","teeest desc"))
-  res.json(await managBook.editBook(54, { title: "book 4444", author: "Abdallah", description: "abdallah book4" }))
+  //res.json(await managBook.editBook(54, { title: "book 4444", author: "Abdallah", description: "abdallah book4" }))
   // res.json(await managBook.search("1"))
 });
 
